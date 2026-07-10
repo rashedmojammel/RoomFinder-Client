dda6c9449b4d533cb5f3a6dcf4b9c582f4f70d60
rashedmojammel <rashedmojamme56@gmail.com>
2026-07-11 03:11:49 +0600

    feat : added list form in owner dashboard

---
+++ src/lib/room-options.ts
@@ -0,0 +1,24 @@
+import type { RoomType } from "@/types/room";
+
+export const roomTypes: RoomType[] = [
+  "Single Room",
+  "Shared Room",
+  "Studio",
+  "Apartment",
+  "Family Suite",
+];
+
+export const amenityOptions: string[] = [
+  "WiFi",
+  "Attached Bathroom",
+  "Air Conditioning",
+  "Furnished",
+  "Balcony",
+  "Parking",
+  "Generator Backup",
+  "24/7 Security",
+  "Gas Connection",
+  "Laundry",
+  "Elevator",
+  "CCTV",
+];
